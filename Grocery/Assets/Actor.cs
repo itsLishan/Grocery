5605b2a9e7133a626d98c11c8360b1767a918284
lemib <lemi.b.lemi@gmail.com>
2014-11-17 10:39:18 -0800

    basic gaze tracking with one cylinder NPC

---
+++ Grocery/Assets/Actor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : SeeableObject {
+
+	// Use this for initialization
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 if (this.gameObject.GetComponent<SeeableObject>().beingLookedAt) {
+						this.renderer.material.color = Color.red;
+				}
+				else
+						this.renderer.material.color = Color.blue;
+	}
+}
---
+++ Grocery/Assets/CheckGaze.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckGaze : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform cameraTransform = null;
+	public GameObject lastHit;
+	float length = 30f;
+	public float radius = 0.3f; //smaller radii work best (sphere is hollow)
+	Vector3 adjustment = new Vector3 (0, 0, 0); //OFF
+	//Vector3 adjustment = new Vector3 (0, .1f, 0); //ON
+	RaycastHit hit;
+	Vector3 rayStart;
+	Vector3 rayDirection;
+	
+	void Start() {
+		cameraTransform = GameObject.FindWithTag("MainCamera").transform;
+	}
+	
+	void Update() {
+		
+		rayDirection = cameraTransform.TransformDirection(Vector3.forward) + adjustment;
+		rayStart = cameraTransform.position;
+		Debug.DrawRay(rayStart, rayDirection * length, Color.green);
+
+		if (Physics.SphereCast(rayStart, radius, rayDirection * length, out hit, Mathf.Infinity)){
+			if (hit.collider.gameObject.GetComponent<SeeableObject>()){
+				if (hit.collider.gameObject.GetComponent<SeeableObject>() != lastHit){
+					lastHit = hit.collider.gameObject;
+					hit.collider.gameObject.GetComponent<SeeableObject>().beingLookedAt = true;
+				}
+
+			} else{
+				if (lastHit != null){
+					lastHit.GetComponent<SeeableObject>().beingLookedAt = false;
+					lastHit = null;
+				}
+			}
+		}
+
+	}
+	
+	void OnDrawGizmos(){
+		Gizmos.color = Color.white;
+		for (int i = 1; i < length; i += 2) {
+			Gizmos.DrawWireSphere (rayStart + (rayDirection * i), radius);
+		}
+	}
+
+}
