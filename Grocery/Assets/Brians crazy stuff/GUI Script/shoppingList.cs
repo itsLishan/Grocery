9b7b1168f320fd9afc73ef0183fb052b8280a129
Brian Pickens <brianpickens53@gmail.com>
2014-11-18 00:32:04 -0800

    Just added some colors

--- Grocery/Assets/Brians crazy stuff/GUI Script/shoppingList.cs
+++ Grocery/Assets/Brians crazy stuff/GUI Script/shoppingList.cs
@@ -5,20 +5,20 @@ public class shoppingList : MonoBehaviour {
 	bool showList = false;
 	// Use this for initialization
 	void Start () {
-
+		Screen.showCursor = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (Input.GetKeyDown(KeyCode.Return))
 		{
 			showList = !showList;
 		}
 	}
 	void OnGUI () {
-		// Make a background box
 
-
+		//GUI.Box (new Rect (Screen.width/2,Screen.height/2,10,10),"");
 
 		if (showList) {
 				GUI.Box (new Rect (10, 10, 100, 300), "Shopping List");
