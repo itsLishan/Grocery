0fde6fbaec418d552ef37f9ea31442e039d1784d
Bethany <bkmart87@gmail.com>
2014-11-30 23:38:07 -0800

    All the items on the shelf

---
+++ Grocery/Assets/SortingHack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SortingHack : MonoBehaviour
+{
+	public const string LAYER_NAME = "I";
+	public int sortingOrder = 3;
+	private SpriteRenderer sprite;
+	
+	void Start()
+	{
+		sprite = GetComponent<SpriteRenderer>();
+		
+		if (sprite)
+		{
+			sprite.sortingOrder = sortingOrder;
+			sprite.sortingLayerName = LAYER_NAME;
+		}
+	}
+}
