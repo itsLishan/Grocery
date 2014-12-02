d6e5c07f2f09920215f50f2e8f6dd8ff0235ae42
Brian Pickens <brianpickens53@gmail.com>
2014-12-02 13:40:38 -0800

    Fixed animations stuff and added boxes

---
+++ Grocery/Assets/Brians crazy stuff/IntroPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroPopup : MonoBehaviour {
+	public Texture2D Intro_popup;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GUI.Label (new Rect ((Screen.width / 3) * 2, Screen.height / 20, 400, 400), Intro_popup);
+	}
+}
---
+++ Grocery/Assets/Brians crazy stuff/ShoppingList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoppingList : MonoBehaviour {
+	public Texture2D ItemList2;
+	bool shoppingList = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Input.GetKeyDown ("space")) {
+			if(!shoppingList)
+			{
+				shoppingList = true;
+			}
+			else{
+				shoppingList = false;
+			}
+				}
+	}
+	void OnGUI(){
+		if (shoppingList) {
+						GUI.Label (new Rect ((Screen.width / 3) * 2, Screen.height / 20, 400, 400), ItemList2);
+				}
+	}
+}
