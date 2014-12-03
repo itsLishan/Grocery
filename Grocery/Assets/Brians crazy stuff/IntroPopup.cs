5df513b3567cbcaa847da6e199245c35c3df91af
Brian Pickens <brianpickens53@gmail.com>
2014-12-02 19:17:32 -0800

    Counters, extra aisle, and camera position

--- Grocery/Assets/Brians crazy stuff/IntroPopup.cs
+++ Grocery/Assets/Brians crazy stuff/IntroPopup.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class IntroPopup : MonoBehaviour {
-
+	public Texture2D Intro;
+	bool introImage = true;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			if(introImage){
+				introImage = false;
+			}
+		}
+		
+	}
 	
+	void OnGUI(){
+		if (introImage) {
+			GUI.Label (new Rect (100,Screen.height/3, 1000, 1000), Intro);
+		}
 	}
 }
