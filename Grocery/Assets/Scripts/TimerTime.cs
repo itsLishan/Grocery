2b5607cdff77d18720b431cf7b14b850004b2d9d
Brian Pickens <brianpickens53@gmail.com>
2014-12-03 01:56:04 -0800

    Hurt Back standing up

--- Grocery/Assets/Scripts/TimerTime.cs
+++ Grocery/Assets/Scripts/TimerTime.cs
@@ -4,26 +4,27 @@ using System.Collections;
 public class TimerTime : MonoBehaviour {
 	public float Seconds = 59;
 	public float Minutes = 0;
+	bool start = false;
 
 	void Update () 
 	{
-		if (Seconds <= 0) { 
-						Seconds = 59;
-						if (Minutes >= 1) 
-			            { 
-							Minutes --;
-						} 
-			             else 
-			            {
-								Minutes = 0;
-								Seconds = 0;
-				GameObject.Find("TimerText").guiText.text = Minutes.ToString("f0") + ":" + Seconds.ToString("f0");
-			 }
-	    }
-		else 
-		{ 
-			Seconds -= Time.deltaTime;
+		if (Input.GetKeyDown ("d")){
+			start = true;
 		}
+		if (start) {//delete me
+						if (Seconds <= 0) { 
+								Seconds = 59;
+								if (Minutes >= 1) { 
+										Minutes --;
+								} else {
+										Minutes = 0;
+										Seconds = 0;
+										GameObject.Find ("TimerText").guiText.text = Minutes.ToString ("f0") + ":" + Seconds.ToString ("f0");
+								}
+						} else { 
+								Seconds -= Time.deltaTime;
+						}
+				}//delete me
 		if (Mathf.Round (Seconds) <= 9) 
 		{
 		     GameObject.Find("TimerText").guiText.text = Minutes.ToString("f0") + ":" + Seconds.ToString("f0");
@@ -32,5 +33,6 @@ public class TimerTime : MonoBehaviour {
 		{
 			GameObject.Find("TimerText").guiText.text = Minutes.ToString("f0") + ":" + Seconds.ToString("f0");
 		}
+		}
 	}
-}
+
