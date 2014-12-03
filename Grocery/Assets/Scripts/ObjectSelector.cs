610b215e3e9bdd97f862cbab81127df98005d5bc
lemib <lemi.b.lemi@gmail.com>
2014-12-02 20:54:09 -0800

    click object

---
+++ Grocery/Assets/Scripts/ObjectSelector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSelector : MonoBehaviour {
+
+	// Use this for initialization
+	private RaycastHit hit;
+	private Ray ray;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0f);
+
+		if (Input.GetMouseButtonDown(0)){
+			if(Physics.Raycast(ray, out hit)){
+				print ("something hit: " + hit.collider.gameObject.name);
+				if (hit.collider.gameObject.tag == "Destroyable"){
+					print(hit.collider.gameObject.name);
+				}
+			}
+		}
+	
+	}
+}
