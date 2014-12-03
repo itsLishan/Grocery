9ebfbb2029c194dc51649b954c0ac806b62e3218
Bethany <bkmart87@gmail.com>
2014-12-02 23:00:47 -0800

    Playing around with animator, trying to get it to trigger animations

--- Grocery/Assets/Scripts/Player2.cs
+++ Grocery/Assets/Scripts/Player2.cs
@@ -6,6 +6,13 @@ public class Player2 : MonoBehaviour {
 	public float speed = 10;
 	private CharacterController controller;
 	private Vector3 moveDirection;
+
+	Animator anim;
+
+	void Start() 
+	{
+	anim = GetComponent<Animator> ();
+		}
 	
 	
 	void Awake (){
@@ -19,15 +26,26 @@ public class Player2 : MonoBehaviour {
 	}
 	void Movement()
 	{    
-		if (Input.GetKey (KeyCode.D))
+		anim.SetFloat ("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+		if (Input.GetAxisRaw ("Horizontal") >0)
 		{
 			transform.Translate(Vector3.right * 20 * Time.deltaTime);
 			transform.eulerAngles = new Vector3(0, 360 ,0); 
 		}
-		if (Input.GetKey (KeyCode.A))
+		if (Input.GetAxisRaw ("Horizontal") <0)
 		{
 			transform.Translate(Vector3.right * 20 * Time.deltaTime);
 			transform.eulerAngles = new Vector3(0,180 ,0); 
+		}
+		if (Input.GetKey (KeyCode.W))
+		{
+			transform.Translate(Vector3.up * 20 * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 0 ,0); 
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			transform.Translate(-Vector3.up * 20 * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 0 ,0); 
 		}
 		controller.Move (moveDirection * Time.deltaTime);
 	}
