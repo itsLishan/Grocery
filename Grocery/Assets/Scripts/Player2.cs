49e9b2108630d9f5c175fcb2820f2c4e24cfe3b6
Bethany <bkmart87@gmail.com>
2014-12-02 21:52:36 -0800

    I made a 2d flipping script. I couldn't figure out how to apply it to a 2.5 D movement script. :( So I consider this 1/2 done

---
+++ Grocery/Assets/Scripts/Player2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+	
+	public float speed = 10;
+	private CharacterController controller;
+	private Vector3 moveDirection;
+	
+	
+	void Awake (){
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	void Update() 
+	{ 
+		Movement ();
+
+	}
+	void Movement()
+	{    
+		if (Input.GetKey (KeyCode.D))
+		{
+			transform.Translate(Vector3.right * 20 * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 360 ,0); 
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.Translate(Vector3.right * 20 * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0,180 ,0); 
+		}
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+	
+	
+}
